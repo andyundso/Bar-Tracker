f693e77bac202c7b3b4bab48e270c0e91fab5494
Andy Pfister <andy.pfister@hotmail.ch>
2019-11-25 19:56:06 +0100

    Added Database Connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import * as Koa from 'koa';
 import {barController} from "./controllers/bar.controller";
+import {databaseConnection} from "./configuration/database";
+
+const PORT: number = Number(process.env.PORT) || 3000;
 
 const app = new Koa();
 
 app.use(barController.routes);
 app.use(barController.allowedMethods);
 
-app.listen(3000);
+databaseConnection
+    .then(() => app.listen(PORT))
+    .catch(console.error);
 
 console.log('Server running on port 3000');
 
