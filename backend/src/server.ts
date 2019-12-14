556bf4ed98e01fffb771b51494999167c46d74b9
Andy Pfister <andy.pfister@hotmail.ch>
2019-12-14 21:05:33 +0100

    Correctly hooked Koa Bodyparser into application

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import * as Koa from 'koa';
 import {barController} from "./controllers/bar.controller";
+import * as bodyParser from "koa-bodyparser";
 
 const innerApp = new Koa();
+
+innerApp.use(bodyParser());
 
 innerApp.use(barController.routes());
 innerApp.use(barController.allowedMethods());
