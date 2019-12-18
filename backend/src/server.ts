541e1d7040a09af93ca5809163db70d88669c113
Andy Pfister <andy.pfister@hotmail.ch>
2019-12-18 20:30:21 +0100

    Added Koa CORS middleware to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import * as Koa from 'koa';
 import {barController} from "./controllers/bar.controller";
 import * as bodyParser from "koa-bodyparser";
+import * as CORS from "@koa/cors"
 
 const innerApp = new Koa();
 
 innerApp.use(bodyParser());
+innerApp.use(CORS());
 
 innerApp.use(barController.routes());
 innerApp.use(barController.allowedMethods());
